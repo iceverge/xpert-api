b7adadec26e8c75cb7b8d91def8796a49def83aa
Vergel Aranas <icevergel@gmail.com>
2023-06-01 09:27:43 +0800

    testing

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -17,77 +17,78 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Check if the API key is provided in the request headers
-  const apiKey: string | undefined = req.headers["x-api-key"]?.toString();
-
-  // Verify if the API key is valid
-  if (!apiKey) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  // Verify if the provided apiKey matches any of the bcrypt hashed keys
-  const isKeyValid = verifyApiKey(apiKey);
-  if (!isKeyValid) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
   return res.status(201).json({ message: "Something" });
+  // // Check if the API key is provided in the request headers
+  // const apiKey: string | undefined = req.headers["x-api-key"]?.toString();
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+  // // Verify if the API key is valid
+  // if (!apiKey) {
+  //   return res.status(401).json({ message: "Unauthorized" });
+  // }
 
-  const {
-    firstName,
-    middleName,
-    lastName,
-    birthdate,
-    gender,
-    email,
-    password,
-  }: Body = req.body;
+  // // Verify if the provided apiKey matches any of the bcrypt hashed keys
+  // const isKeyValid = verifyApiKey(apiKey);
+  // if (!isKeyValid) {
+  //   return res.status(401).json({ message: "Unauthorized" });
+  // }
 
-  if (!password) {
-    return res.status(400).json({ message: "Password is required" });
-  }
+  // return res.status(201).json({ message: "Something" });
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+  // if (req.method !== "POST") {
+  //   return res.status(405).json({ message: "Method not allowed" });
+  // }
 
-    const profileURL: string =
-      gender === "male"
-        ? "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
-        : "https://static.vecteezy.com/system/resources/previews/006/898/692/non_2x/avatar-face-icon-female-social-profile-of-business-woman-woman-portrait-support-service-call-center-illustration-free-vector.jpg";
+  // const {
+  //   firstName,
+  //   middleName,
+  //   lastName,
+  //   birthdate,
+  //   gender,
+  //   email,
+  //   password,
+  // }: Body = req.body;
 
-    const user = await prisma.user.create({
-      data: {
-        firstName,
-        middleName,
-        lastName,
-        birthdate,
-        gender,
-        email,
-        createdAt: new Date(),
-        account: {
-          create: {
-            password: hashedPassword,
-            createdAt: new Date(),
-          },
-        },
-        talent: {
-          create: {
-            profileURL: profileURL,
-            createdAt: new Date(),
-          },
-        },
-      },
-    });
+  // if (!password) {
+  //   return res.status(400).json({ message: "Password is required" });
+  // }
 
-    return res
-      .status(201)
-      .json({ message: "User registered successfully", user });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Error registering user" });
-  }
+  // try {
+  //   const hashedPassword = await bcrypt.hash(password, 10);
+
+  //   const profileURL: string =
+  //     gender === "male"
+  //       ? "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
+  //       : "https://static.vecteezy.com/system/resources/previews/006/898/692/non_2x/avatar-face-icon-female-social-profile-of-business-woman-woman-portrait-support-service-call-center-illustration-free-vector.jpg";
+
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       firstName,
+  //       middleName,
+  //       lastName,
+  //       birthdate,
+  //       gender,
+  //       email,
+  //       createdAt: new Date(),
+  //       account: {
+  //         create: {
+  //           password: hashedPassword,
+  //           createdAt: new Date(),
+  //         },
+  //       },
+  //       talent: {
+  //         create: {
+  //           profileURL: profileURL,
+  //           createdAt: new Date(),
+  //         },
+  //       },
+  //     },
+  //   });
+
+  //   return res
+  //     .status(201)
+  //     .json({ message: "User registered successfully", user });
+  // } catch (error) {
+  //   console.error(error);
+  //   return res.status(500).json({ message: "Error registering user" });
+  // }
 }
