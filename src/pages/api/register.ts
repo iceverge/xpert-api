c65753c769f3dd3536ae3a5739646e825b7ec4e9
Vergel Aranas <icevergel@gmail.com>
2023-06-03 01:15:51 +0800

    Debugging

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
+import bodyParser from "body-parser";
 import prisma from "@lib/prisma";
 import { verifyApiKey } from "@lib/header";
 
@@ -13,6 +14,15 @@ type Body = {
   email: string;
   password: string;
 };
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// Parse the request body
+const jsonParser = bodyParser.json();
 
 export default async function handler(
   req: NextApiRequest,
@@ -31,68 +41,65 @@ export default async function handler(
   // if (!isKeyValid) {
   //   return res.status(401).json({ message: "Unauthorized" });
   // }
+  jsonParser(req, res, async () => {
+    if (req.method !== "POST") {
+      return res.status(405).json({ message: "Method not allowed" });
+    }
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+    const {
+      firstName,
+      middleName,
+      lastName,
+      birthdate,
+      gender,
+      talent,
+      email,
+      password,
+    }: Body = req.body;
 
-  const {
-    firstName,
-    middleName,
-    lastName,
-    birthdate,
-    gender,
-    talent,
-    email,
-    password,
-  }: Body = req.body;
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
+    }
 
-  // if (!password) {
-  //   return res.status(400).json({ message: "Password is required" });
-  // }
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-  // return res
-  //   .status(201)
-  //   .json({ message: "User registered successfully", email });
+      const profileURL: string =
+        gender.toLowerCase() === "male"
+          ? "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
+          : "https://static.vecteezy.com/system/resources/previews/006/898/692/non_2x/avatar-face-icon-female-social-profile-of-business-woman-woman-portrait-support-service-call-center-illustration-free-vector.jpg";
 
-  try {
-    // const hashedPassword = await bcrypt.hash(password, 10);
-
-    const profileURL: string =
-      gender.toLowerCase() === "male"
-        ? "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
-        : "https://static.vecteezy.com/system/resources/previews/006/898/692/non_2x/avatar-face-icon-female-social-profile-of-business-woman-woman-portrait-support-service-call-center-illustration-free-vector.jpg";
-
-    const user = await prisma.user.create({
-      data: {
-        firstName,
-        middleName,
-        lastName,
-        birthdate,
-        gender,
-        email,
-        createdAt: new Date(),
-        account: {
-          create: {
-            password: password,
-            createdAt: new Date(),
+      const user = await prisma.user.create({
+        data: {
+          firstName,
+          middleName,
+          lastName,
+          birthdate,
+          gender,
+          email,
+          createdAt: new Date(),
+          account: {
+            create: {
+              password: hashedPassword,
+              createdAt: new Date(),
+            },
+          },
+          talent: {
+            create: {
+              profileURL: profileURL,
+              talentType: talent,
+              createdAt: new Date(),
+            },
           },
         },
-        talent: {
-          create: {
-            profileURL: profileURL,
-            talentType: talent,
-            createdAt: new Date(),
-          },
-        },
-      },
-    });
+      });
 
-    return res
-      .status(201)
-      .json({ message: "User registered successfully", user });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Error registering user" });
-  }
+      return res
+        .status(201)
+        .json({ message: "User registered successfully", user });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Error registering user" });
+    }
+  });
 }
