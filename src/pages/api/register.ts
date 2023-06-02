aa76b70c3217fb709cce84b1dea711c66e9791c8
Vergel Aranas <icevergel@gmail.com>
2023-06-03 01:03:13 +0800

    debugging

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -47,52 +47,52 @@ export default async function handler(
     password,
   }: Body = req.body;
 
-  if (!password) {
-    return res.status(400).json({ message: "Password is required" });
-  }
-
-  return res
-    .status(201)
-    .json({ message: "User registered successfully", email });
-
-  // try {
-  //   const hashedPassword = await bcrypt.hash(password, 10);
-
-  //   const profileURL: string =
-  //     gender.toLowerCase() === "male"
-  //       ? "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
-  //       : "https://static.vecteezy.com/system/resources/previews/006/898/692/non_2x/avatar-face-icon-female-social-profile-of-business-woman-woman-portrait-support-service-call-center-illustration-free-vector.jpg";
-
-  //   const user = await prisma.user.create({
-  //     data: {
-  //       firstName,
-  //       middleName,
-  //       lastName,
-  //       birthdate,
-  //       gender,
-  //       email,
-  //       createdAt: new Date(),
-  //       account: {
-  //         create: {
-  //           password: hashedPassword,
-  //           createdAt: new Date(),
-  //         },
-  //       },
-  //       talent: {
-  //         create: {
-  //           profileURL: profileURL,
-  //           talentType: talent,
-  //           createdAt: new Date(),
-  //         },
-  //       },
-  //     },
-  //   });
-
-  //   return res
-  //     .status(201)
-  //     .json({ message: "User registered successfully", user });
-  // } catch (error) {
-  //   console.error(error);
-  //   return res.status(500).json({ message: "Error registering user" });
+  // if (!password) {
+  //   return res.status(400).json({ message: "Password is required" });
   // }
+
+  // return res
+  //   .status(201)
+  //   .json({ message: "User registered successfully", email });
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const profileURL: string =
+      gender.toLowerCase() === "male"
+        ? "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
+        : "https://static.vecteezy.com/system/resources/previews/006/898/692/non_2x/avatar-face-icon-female-social-profile-of-business-woman-woman-portrait-support-service-call-center-illustration-free-vector.jpg";
+
+    const user = await prisma.user.create({
+      data: {
+        firstName,
+        middleName,
+        lastName,
+        birthdate,
+        gender,
+        email,
+        createdAt: new Date(),
+        account: {
+          create: {
+            password: hashedPassword,
+            createdAt: new Date(),
+          },
+        },
+        talent: {
+          create: {
+            profileURL: profileURL,
+            talentType: talent,
+            createdAt: new Date(),
+          },
+        },
+      },
+    });
+
+    return res
+      .status(201)
+      .json({ message: "User registered successfully", user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error registering user" });
+  }
 }
